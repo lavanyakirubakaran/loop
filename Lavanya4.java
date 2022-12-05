c296a56381a9e614304f790a6960366ea564eaa6
lavanyarajendran <116621788+lavanyakirubakaran@users.noreply.github.com>
2022-12-05 15:20:50 +0530

    Add files via upload

---
+++ Lavanya4.java
@@ -0,0 +1,19 @@
+package com.java1;
+
+public class Lavanya4 {
+	public static void main (String lava[])
+	{
+		for(int i=1;i<=3;i++) {
+			for(int j=1;j<i;j++) {
+				System .out.print("@");
+				
+			}for(int g=3;g>i;g--) {
+				System .out.print("$");
+				
+			}
+			System .out.println();
+		}
+	}
+}
+
+
